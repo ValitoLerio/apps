04f911ece2d2877efae931254aa673467b123214
Valeriano Familiar Fraile <vff.ad@hotmail.com>
2026-09-20 11:05:40 +0200

    Fotos: album nuevo. Las fotos se quedan en sus carpetas y aqui va su ficha

--- sync.js
+++ sync.js
@@ -44,7 +44,8 @@ var SECCIONES = {
   patrimonio: ['patrimonio.libro.v1'],
   pedidos: ['pedidos.libro.v1'],
   vacaciones: ['vacaciones.libro.v1'],
-  recetas: ['recetas.libro.v1']
+  recetas: ['recetas.libro.v1'],
+  fotos:   ['fotos.libro.v1']
 };
 
 var seccionActiva = null;
